f2aee788ba17d5a4eed2f2d5b299c849858d6d93
Timo Sand <timo.sand@kapsi.fi>
2010-07-01 15:35:10 +0300

    Ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100701114914) do
+ActiveRecord::Schema.define(:version => 20100701122304) do
+
+  create_table "announcements", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.boolean  "notification", :default => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "news", :force => true do |t|
     t.string   "title"

739f7e4127e2d9c1f651876b49a6c46d86d63ff7
Timo Sand <timo.sand@kapsi.fi>
2010-07-14 14:25:53 +0300

    Got static pages working again and added validation

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,6 @@
 class StaticController < ApplicationController
 
-  rescue_from ActionView::MissingTemplate, :with => :invalid_page
+  before_filter :ensure_valid, :except => :index
 
   def index
     @news = Announcement.find_all_by_notification(false, :order => "created_at DESC")
@@ -8,13 +8,25 @@ class StaticController < ApplicationController
   end
 
   def show
-    render :template => params[:page]
+    render :template => current_page # params[:page]
   end
 
 protected
 
-  def invalid_page
-    render :nothing => true, :status => 404
+  def ensure_valid
+    unless template_exists?(current_page)
+      render :nothing => true, :status => 404 and return false
+    end
+  end
+
+  def current_page
+    "static/#{File.join(*params[:page]).downcase}"
+  end
+
+  def template_exists?(path)
+    view_paths.find_template(path, response.template.template_format)
+  rescue ActionView::MissingTemplate
+    false
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
 
+  map.root :controller => 'static'
+
   map.resources :trimmers, :as => 'trimmaajat'
   map.resources :kennels, :as => 'kasvattajat'
   map.resources :events , :as => 'tapahtumat'
@@ -10,25 +12,11 @@ ActionController::Routing::Routes.draw do |map|
     :action       => 'show',
     :requirements => { :page => /[a-z]+/ }
 
-  map.connect 'tapahtumat/:id',
-    :controller => 'events',
-    :action => 'show',
-    :requirements => { :id => /[0-9]+/ }
-
-  map.root :controller => 'static'
-
   # The priority is based upon order of creation: first created -> highest priority.
-
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
   #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
   # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
 
   # Sample resource route with options:
   #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
